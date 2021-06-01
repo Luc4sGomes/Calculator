2ebd065cca7d80ccdebc3ed2c675c4b214d68c9d
lucas <lucasgominhaa448@gmail.com>
2021-06-01 16:16:45 -0300

    added final arrows functions in script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,33 +1,60 @@
-function Calculator(){
-    this.input = document.querySelector('#input');
-    
-    this.start = () => this.catchClicks();
+function Calculator() {
+  this.input = document.querySelector("#input");
 
-    this.catchClicks = () =>{
-        document.addEventListener('click',event =>{
-            const element = event.target;
+  this.start = () => {
+    this.catchClicks();
+    this.catchEnter();
+  };
 
-            if(element.classList.contains('btn-num')){
-                this.addNumberInput(element);
-            }
-            if(element.classList.contains('clear')){
-                this.clear();
-            }
-            if(element.classList.contains('delete')){
-                this.del(element);
-            }
-            if(element.classList.contains('equal')){
-                this.makeCount(element);
-            }
+  this.catchClicks = () => {
+    document.addEventListener("click", (event) => {
+      const element = event.target;
 
-        });
+      if (element.classList.contains("btn-num")) {
+        this.addNumberInput(element);
+      }
+      if (element.classList.contains("clear")) {
+        this.clear();
+      }
+      if (element.classList.contains("delete")) {
+        this.del(element);
+      }
+      if (element.classList.contains("equal")) {
+        this.makeCount(element);
+      }
+    });
+  };
+
+  this.addNumberInput = (element) => {
+    this.input.value += element.innerText;
+    this.input.focus();
+  };
+
+  this.clear = () => (this.input.value = "");
+
+  this.del = () => (this.input.value = this.input.value.slice(0, -1));
+
+  this.makeCount = () => {
+    try {
+      const conta = eval(this.input.value);
+
+      if (!conta) {
+        alert("conta invalida");
+        return;
+      }
+      this.input.value = conta;
+    } catch (error) {
+      alert("conta invalida");
+      return;
     }
+  };
 
-    this.clear = () => this.input.value = "";
-    
-
-    this.addNumberInput = element => this.input.value += element.innerText;
-
+  this.catchEnter = () => {
+    document.addEventListener("keyup", (event) => {
+      if (event.keyCode === 13) return;
+      this.makeCount();
+    });
+  };
 }
 
 const calculadora = new Calculator();
